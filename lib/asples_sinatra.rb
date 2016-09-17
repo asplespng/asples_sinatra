9c7fd134d44db678c183688d01f382d7be354387
Per Evensen <per.evensen@gmail.com>
2016-09-16 23:55:27 -0400

    fixed template

--- lib/asples_sinatra.rb
+++ lib/asples_sinatra.rb
@@ -6,5 +6,6 @@ require 'active_support/core_ext'
 
 
 module AsplesSinatra
+  Sinatra::Application.set :views, Proc.new { File.join(root, "lib/asples_sinatra/templates/") }
   # Your code goes here...
 end
