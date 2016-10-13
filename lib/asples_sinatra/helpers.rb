2db60a2417c9ea7434525df9c76b656388f77303
Per Evensen <per.evensen@gmail.com>
2016-10-12 21:44:37 -0400

    moved template

--- lib/asples_sinatra/helpers.rb
+++ lib/asples_sinatra/helpers.rb
@@ -1,21 +1,9 @@
-enable :inline_templates
 module AsplesSinatra
   module Helpers
     def input(name, options={})
-      haml :'input', {layout: false}, options.merge!({field_name: name})
+      template_path = File.join(File.dirname(File.expand_path(__FILE__)), '/templates/input.haml')
+      tmpl = Tilt.new(template_path)
+      tmpl.render(self, options.merge!({field_name: name}))
     end
   end
 end
-
-
-__END__
-
-@@ input
-%div{class: "form-group #{object.respond_to?(:errors) && object.errors.has_key?(field_name) && object.errors[field_name].any? && "has-error"}"}
-  -field_type = defined?(as) && as.present? ? as : :text
-  -field_label = defined?(label) && label.present? ? label : field_name.to_s.humanize
-  -field_placeholder = defined?(placeholder) && placeholder.present? ? placeholder : "Enter #{field_name.to_s.humanize.downcase}"
-  %label.control-label{for: field_name}="#{field_label}:"
-  %input{type: field_type, name: field_name, class: "form-control", placeholder: field_placeholder, value: object.send(field_name)}
-  %span.help-block
-    = object.errors[field_name].join('. ') if object.respond_to?(:errors) && object.errors.has_key?(field_name) && object.errors.any?
